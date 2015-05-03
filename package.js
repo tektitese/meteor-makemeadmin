3fc12189872038630809522b1509745c750305d4
Max Malm <benjick@dumfan.net>
2015-05-03 12:46:58 +0200

    version constraint for package alanning:roles

--- package.js
+++ package.js
@@ -8,7 +8,7 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.use(['templating','tracker'], 'client');
-  api.use('alanning:roles', {weak: true});
+  api.use('alanning:roles@1.2.0', {weak: true});
   
   api.versionsFrom('1.1.0.2');
   api.addFiles(['makemeadmin.html','makemeadmin.js'], 'client');
